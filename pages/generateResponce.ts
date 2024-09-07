7d725621499049922d9097686ba0d060e52355d8
Santhosh29k <thanush9524@gmail.com>
2024-09-07 16:49:05 +0530

    gr

--- pages/generateResponce.ts
+++ pages/generateResponce.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import openai from '../../lib/openai';
+import openai from '../lib/openai';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
